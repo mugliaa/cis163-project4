2e5f4a2369e145a2c6bd6318e15f710eb354b149
Adam Muglia <mugliaa@mail.gvsu.edu>
2015-11-17 18:50:08 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,30 @@
+package project4;
+
+public class Node<E> {
+	public E data;
+	public Node<E> next;
+	
+	public Node(E data, Node<E> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public Node() {
+	}
+
+	public E getData() {
+		return data;
+	}
+	
+	public void setData(E data) {
+		this.data = data;
+	}
+	
+	public Node<E> getNext() {
+		return next;
+	}
+	
+	public void setNext(Node<E> next) {
+		this.next = next;
+	}
+}
