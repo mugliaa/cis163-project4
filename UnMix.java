0a546dcf9d1da083481b3b567982feb7c93c0a2b
Adam Muglia <mugliaa@mail.gvsu.edu>
2015-11-17 18:48:41 -0500

    Create UnMix.java

---
+++ UnMix.java
@@ -0,0 +1,11 @@
+package project4;
+
+public class UnMix implements iUnMix {
+
+	@Override
+	public String UnMixUsingFile(String filename, String userMessage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
