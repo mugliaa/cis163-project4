28e789f8390384e5cd5e42c75a4c348dd89870e1
Adam Muglia <mugliaa@mail.gvsu.edu>
2015-11-18 17:33:55 -0500

    Update Mix.java

--- Mix.java
+++ Mix.java
@@ -25,12 +25,16 @@ public class Mix implements iMix {
 			System.out.println("Closing program...");
 			System.out.println("Final Message:");
 			secretMessage.display();
+			s.close();
+			return null;
 		}
 		
 		// User wants to insert a character before the specified position.
 		if (command.startsWith("b")) {
 			// Format: b c #
 			commands.push(command);
+			s.close();
+			return null;
 		}
 		
 		// User wants to remove a character at the specified position.
@@ -136,17 +140,135 @@ public class Mix implements iMix {
 			s.close();
 			return null;
 		}
-		
+
 		// User wants to switch characters at the two specified positions.
 		if (command.startsWith("w")) {
 			// Format: w & #
+			String[] data = command.split(" ");
+
+			if (data.length == 1 || data.length == 2) {
+				System.out.println("Invalid command entered! Try again!");
+				System.out.println("");
+				secretMessage.display();
+				System.out.println("");
+				System.out.println("");
+				printCommandListing();
+				System.out.println("");
+				System.out.println("Command: ");
+				String c = s.nextLine();
+				System.out.println("");
+				processCommand(c);
+				s.close();
+				return null;
+			}
+
+			// Checking if command was properly input
+			if (data[1].equals(null) || data[2].equals(null)) {
+				System.out.println("Invalid command entered! Try again!");
+				System.out.println("");
+				secretMessage.display();
+				System.out.println("");
+				System.out.println("");
+				printCommandListing();
+				System.out.println("");
+				System.out.println("Command: ");
+				String c = s.nextLine();
+				System.out.println("");
+				processCommand(c);
+				s.close();
+				return null;
+			}
+
+			if (data[1].equals("") || data[1].equals(" ") || data[2].equals("") || data[2].equals(" ")) {
+				System.out.println("Invalid command entered! Try again!");
+				System.out.println("");
+				secretMessage.display();
+				System.out.println("");
+				System.out.println("");
+				printCommandListing();
+				System.out.println("");
+				System.out.println("Command: ");
+				String c = s.nextLine();
+				System.out.println("");
+				processCommand(c);
+				s.close();
+				return null;
+			}
+
+			if (!isNumeric(data[1]) || !isNumeric(data[2])) {
+				System.out.println("Invalid command entered! Try again!");
+				System.out.println("");
+				secretMessage.display();
+				System.out.println("");
+				System.out.println("");
+				printCommandListing();
+				System.out.println("");
+				System.out.println("Command: ");
+				String c = s.nextLine();
+				System.out.println("");
+				processCommand(c);
+				s.close();
+				return null;
+			}
+
+			int index1 = Integer.parseInt(data[1]);
+			int index2 = Integer.parseInt(data[2]);
+
+			// Checking if index is valid
+			if (index1 < 0 || index1 > secretMessage.count() || 
+					index2 < 0 || index2 > secretMessage.count()) {
+				System.out.println("Invalid position entered! Try again!");
+				System.out.println("");
+				secretMessage.display();
+				System.out.println("");
+				System.out.println("");
+				printCommandListing();
+				System.out.println("");
+				System.out.println("Command: ");
+				String c = s.nextLine();
+				System.out.println("");
+				processCommand(c);
+				s.close();
+				return null;
+			}
+			
+			// TODO: Switch characters here
+			
 			commands.push(command);
+			s.close();
+			return null;
 		}
 		
 		// User wants to save the commands to a file.
 		if (command.startsWith("s")) {
 			// Format: s filename
 			commands.push(command);
+			s.close();
+			return null;
+		}
+		
+		// User wants to cut to the clipboard, starting at & to # (inclusive)
+		if (command.startsWith("x")) {
+			// Format: x & #
+			commands.push(command);
+			s.close();
+			return null;
+		}
+		
+		// User wants to paste from the clipboard starting at #
+		if (command.startsWith("p")) {
+			// Format: p #
+			commands.push(command);
+			s.close();
+			return null;
+		}
+		
+		// User wants to copy to the clipboard starting at & to #
+		if (command.startsWith("c")) {
+			// Format: c & #
+			commands.push(command);
+			s.close();
+			return null;
 		}
 		
 		// Invalid command entered. Prompt user again.
@@ -177,6 +299,9 @@ public class Mix implements iMix {
 		System.out.println("b c # \t\t Insert character (c) before position (#).");
 		System.out.println("r # \t\t Remove the character at position (#).");
 		System.out.println("w & # \t\t Switch character at position & with #.");
+		System.out.println("x & # \t\t Cut to clipboard; starting at position & to #.");
+		System.out.println("p # \t\t Paste from clipboard starting at position #.");
+		System.out.println("c & # \t\t Copy to clipboard starting at position & to #.");
 		System.out.println("s filename \t Save commands to a text file named 'filename'.");
 	}
 	
