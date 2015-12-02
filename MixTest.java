60cfe326e87d5b6164977a0278be2fd3789c5607
Adam Muglia <mugliaa@mail.gvsu.edu>
2015-12-01 23:30:29 -0500

    Update MixTest.java

--- MixTest.java
+++ MixTest.java
@@ -149,6 +149,34 @@ public class MixTest {
 		assertEquals("This is a test", m.secretMessage.displayMessage());
 	}
 	
+	@Test // Tests invalid remove command - 1 under lower bound
+	public void testRemoveInvalid6() {
+		Mix m = new Mix();
+		m.testing = true;
+		m.setInitialMessage("This is a test");
+		m.processCommand("r -1");
+		assertEquals("This is a test", m.secretMessage.displayMessage());
+	}
+	
+	@Test // Tests invalid remove command - 1 above upper bound
+	public void testRemoveInvalid7() {
+		Mix m = new Mix();
+		m.testing = true;
+		m.setInitialMessage("This is a test");
+		m.processCommand("r 14");
+		assertEquals("This is a test", m.secretMessage.displayMessage());
+	}
+	
+	@Test // Tests removing the final character of message - invalid
+	public void testRemoveFinalCharacter() {
+		Mix m = new Mix();
+		m.testing = true;
+		m.setInitialMessage("AM");
+		m.processCommand("r 0");
+		m.processCommand("r 0");
+		assertEquals("M", m.secretMessage.displayMessage());
+	}
+	
 	@Test // Tests cut starting at 0 position
 	public void testCutAtZero() {
 		Mix m = new Mix();
@@ -203,6 +231,15 @@ public class MixTest {
 		assertEquals("This is a test", m.secretMessage.displayMessage());
 	}
 	
+	@Test // Tests invalid cut
+	public void testCutInvalid4() {
+		Mix m = new Mix();
+		m.testing = true;
+		m.setInitialMessage("This is a test");
+		m.processCommand("x 10 3000");
+		assertEquals("This is a test", m.secretMessage.displayMessage());
+	}
+	
 	@Test // Tests cut starting at middle position
 	public void testCutAtEnd() {
 		Mix m = new Mix();
@@ -221,7 +258,7 @@ public class MixTest {
 		assertEquals("This is a est", m.secretMessage.displayMessage());
 	}
 	
-	@Test // Tests cut using inverted positions
+	@Test // Tests cut using inverted positions - invalid
 	public void testCutAtInvertedPositions1() {
 		Mix m = new Mix();
 		m.testing = true;
@@ -230,7 +267,7 @@ public class MixTest {
 		assertEquals("This is a test", m.secretMessage.displayMessage());
 	}
 	
-	@Test // Tests cut using inverted positions
+	@Test // Tests cut using inverted positions - invalid
 	public void testCutAtInvertedPositions2() {
 		Mix m = new Mix();
 		m.testing = true;
@@ -238,5 +275,25 @@ public class MixTest {
 		m.processCommand("x 5 1");
 		assertEquals("This is a test", m.secretMessage.displayMessage());
 	}
+	
+	@Test // Tests cut using the entire message - invalid
+	public void testCutFullMessage() {
+		Mix m = new Mix();
+		m.testing = true;
+		m.setInitialMessage("This is a test");
+		m.processCommand("x 0 13");
+		assertEquals("This is a test", m.secretMessage.displayMessage());
+	}
+	
+	@Test // Tests cut and paste
+	public void testCutAndPaste1() {
+		Mix m = new Mix();
+		m.testing = true;
+		m.setInitialMessage("This is a test");
+		m.processCommand("x 0 4");
+		m.processCommand("p 1");
+		assertEquals("iThis s a test", m.secretMessage.displayMessage());
+	}
+	
 
 }
